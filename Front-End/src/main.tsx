1f376add85c3c767f64bc4d88e42a0253e7e0c1b
doutor-digital <doutordigitalconsultoria@gmail.com>
2026-05-09 03:36:31 -0300

    feat: replace HashRouter with BrowserRouter for improved routing

--- Front-End/src/main.tsx
+++ Front-End/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { registerSW } from "virtual:pwa-register";
@@ -35,10 +35,10 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
         <Toaster position="top-right" richColors />
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
